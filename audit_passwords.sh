53d0f81977e47f2226c1ec1e2e8a4b0e6f40e296
sar <sebastian.alighierir@uanl.edu.mx>
2025-09-15 06:21:43 -0500

    Refactor audit_passwords
    - Add logic to construct unshadow when needed, instead of saving it to a
      temp file.
    - Add logic to use awk for user detection in unshadow files.
    - Add logic to show if user password is weak or not instead of showing
      it.
    - Use env in shebang to make the script more compatible.
    - Add err function for logging.

--- audit_passwords.sh
+++ audit_passwords.sh
@@ -1,31 +1,71 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-rm reporte.txt
+WORDLIST_PATH="./rockyou.txt"
 
-echo "------------------------------------" >> reporte.txt
-echo "|    USUARIO    |    CONTRASEÑA    |" >> reporte.txt
-echo "------------------------------------" >> reporte.txt
-echo "|                                  |" >> reporte.txt
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $*" >&2
+}
 
-sudo unshadow /etc/passwd /etc/shadow > contrasenas.out
-grep '\$' contrasenas.out | tail -n 3 > temp_hashes.out
-cat temp_hashes.out
-john --wordlist=/usr/share/wordlists/rockyou.txt --format=crypt temp_hashes.out
+is_john_installed(){
+  if ! command -v john &>/dev/null; then
+    err "John is not installed"
+    return 1
+  fi
+}
 
-john --show temp_hashes.out > cracked.txt
+get_unshadow(){
+  if [[ ! -f /etc/passwd ]]; then
+    err "Missing /etc/passwd file"
+    return 1
+  fi
 
-cut -d: -f1 temp_hashes.out > usuarios_nombres.txt
+  if [[ ! -f /etc/shadow ]]; then
+    err "Missing /etc/passwd file"
+    return 1
+  fi
 
-cut -d: -f1 cracked.txt > usuarios_debiles.txt
+  # Regular users have an ID greater than 1000 and a login shell defined
+  sudo unshadow /etc/passwd /etc/shadow \
+    | awk --field-separator=':' '$3 >= 1000 && $7 ~ /^(\/(bin|usr\/bin)\/(bash|sh|zsh|fish))$/'
+}
 
-while read -r usuario; do
-    echo "$usuario"
-    if grep -q "$usuario" usuarios_debiles.txt; then
-        estado="Debil"
+get_unshadow_users(){
+  get_unshadow | awk --field-separator=':' '$3 >= 1000 && $7 ~ /^(\/(bin|usr\/bin)\/(bash|sh|zsh|fish))$/ {print $1}'
+}
+
+get_unshadow_entries(){
+  get_unshadow | awk --field-separator=':' '$3 >= 1000 && $7 ~ /^(\/(bin|usr\/bin)\/(bash|sh|zsh|fish))$/'
+}
+
+make_john_rip(){
+  local wordlist="$1"
+  john --wordlist="$wordlist" --format=crypt <(get_unshadow_entries)
+}
+
+get_rip_users(){
+  users=()
+
+  while IFS= read -r line; do
+    # break on the first empty line
+    [[ -z "$line" ]] && break 
+    # extract up to the first colon
+    user="${line%%:*}"
+    users+=("$user")
+  done < <(john --show <(get_unshadow))
+
+  echo "${users[@]}"
+}
+
+get_report(){
+  while IFS= read -r user; do
+    if grep --quiet "$user" <(get_rip_users); then
+      echo "$user has been riped"
     else
-        estado="Fuerte"
+      echo "$user hasn't been riped"
     fi
-    echo "|   $usuario   |   $estado   |" >> reporte.txt
-done < usuarios_nombres.txt
+done < <(get_unshadow_users)
+}
 
-cat reporte.txt
+is_john_installed 
+make_john_rip "$WORDLIST_PATH"
+get_report
